ed091ccbe26dd61fe1a2e23d767236089747867a
Matheus Júnior <matheusfpjunior@gmail.com>
2022-04-27 18:02:46 -0300

    Barra de pesquisa adicionada no Header, tentativa de adicioná-la somente nas 2° e 3° páginas.

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,16 +1,46 @@
-import React from 'react';
-/* import styled from 'styled-components' */
+import { TextField, IconButton, InputAdornment } from '@mui/material';
+import React, { useState } from "react";
 import './style.css';
 
-/* const Header = styled.nav`
-    justify-content: space-between;
-`
- */
+import "@fontsource/roboto/500.css";
+import SearchIcon from "@mui/icons-material/Search";
+
+import { Buscar } from "../../API";
+
+import { useLocation } from 'react-router-dom';
+
 function Header() {
+    const [usuario, setUsuario] = useState("");
+    const location = useLocation();
+
+    
     return(
         <nav>
             <h1>Desafio 2</h1>
-            <h1>React APIs</h1>
+
+            {/* Isso só deve ser exibido na 2° e 3° página. */}
+
+            <TextField
+            value={usuario}
+            onChange={(event) => {
+              setUsuario(event.target.value);
+            }}
+            id="standard-basic"
+            label="Pesquise um usuário"
+            variant="outlined"
+            margin="none"
+            className="search"
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton onClick={() => Buscar(usuario)}>
+                    <SearchIcon />
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          />
+          <h1>React APIs</h1>
         </nav>
     );
 }
